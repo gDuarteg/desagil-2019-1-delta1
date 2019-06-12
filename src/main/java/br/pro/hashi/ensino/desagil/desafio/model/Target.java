735bebf464a843a58c527338a9abb562359974b3
Gabriel Duarte <gabrielmd@al.insper.edu.br>
2019-06-12 16:40:33 -0300

    questao1

--- src/main/java/br/pro/hashi/ensino/desagil/desafio/model/Target.java
+++ src/main/java/br/pro/hashi/ensino/desagil/desafio/model/Target.java
@@ -1,10 +1,24 @@
 package br.pro.hashi.ensino.desagil.desafio.model;
 
+import java.util.Random;
+
 public class Target extends Element {
     private final Board board;
+    private final Random mov = new Random();
 
     public Target(int row, int col, Board board) {
         super(row, col);
         this.board = board;
+    }
+
+    // Ele sai da zona do jogo, porem ta movimentando. Merece um D :)
+    public void move() {
+        if (this.col <= 19 && this.row <= 9) {
+            this.row = this.row - mov.nextInt(2);
+            this.col = this.col - mov.nextInt(2);
+        } else if (this.col >= 0 && this.row >= 0) {
+            this.row = this.row + mov.nextInt(2);
+            this.col = this.col + mov.nextInt(2);
+        }
     }
 }
